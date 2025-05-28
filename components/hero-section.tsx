14831c180784032337e0097dbd94654980934572
retailjet <ruben@rjcapital.io>
2025-05-28 23:01:01 +0000

    Update hero badge text to be more concise

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -92,7 +92,7 @@ function HeroSection() {
                                     <Link
                                         href="#integrated-marketplace"
                                         className="hover:bg-background dark:hover:border-t-border bg-muted group mx-auto flex w-fit items-center gap-4 rounded-full border p-1 pl-4 shadow-md shadow-zinc-950/5 transition-colors duration-300 dark:border-t-white/5 dark:shadow-zinc-950">
-                                        <span className="text-foreground text-sm">The ChatGPT Plugin for Businesses</span>
+                                        <span className="text-foreground text-sm">ChatGPT Plugin for Businesses</span>
                                         <span className="dark:border-background block h-4 w-0.5 border-l bg-white dark:bg-zinc-700"></span>
 
                                         <div className="bg-background group-hover:bg-muted size-6 overflow-hidden rounded-full duration-500">
